b9c26eb92bbd3aad560e3d870dbc47b55e1ecf96
Jordan Luse <j.luse7@gmail.com>
2017-10-22 20:20:04 -0700

    Can view individual listings

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,6 @@
 // about supported directives.
 //
 //= require turbolinks
-
 //= require rails-ujs
 //= require react
 //= require react_ujs
--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -2,4 +2,8 @@ class LandingController < ApplicationController
   def index
     @listings = Listing.all
   end
+
+  def show
+    @listing = Listing.find(params[:id])
+  end
 end
